08bd31902a37928cae831632a18b8f8efd1da4a7
Ahammod <ahammodullah12@gmail.com>
2024-11-26 00:45:44 +0600

    first commit

---
+++ app.js
@@ -0,0 +1,47 @@
+let btn1 = document.querySelector('#btn1');
+
+btn1.addEventListener('click', function(){
+
+    let input1 = document.querySelector('#input1').value;
+    let show1 = document.querySelector('#show1');
+    
+    show1.innerHTML = input1;
+    show1.style.color = 'White';
+    console.log(show1);
+});
+
+let btn2 = document.querySelector('#btn2');
+
+btn2.addEventListener('click', function(){
+
+    let input2 = document.querySelector('#input2').value;
+    let show2 = document.querySelector('#show2');
+    
+    show2.innerHTML = input2;
+    show2.style.color = 'White';
+    console.log(show2);
+});
+
+let btn3 = document.querySelector('#btn3');
+
+btn3.addEventListener('click', function(){
+
+    let input3 = document.querySelector('#input3').value;
+    let show3 = document.querySelector('#show3');
+    
+    show3.innerHTML = input3;
+    show3.style.color = 'White';
+    console.log(show3);
+});
+
+let btn4 = document.querySelector('#btn4');
+
+btn4.addEventListener('click', function(){
+
+    let input4 = document.querySelector('#input4').value;
+    let show4 = document.querySelector('#show4');
+    
+    show4.innerHTML = input4;
+    show4.style.color = 'White';
+    console.log(show4);
+});
